f177e95ad7e5db9bf981c40e8d87642d32951053
Sophie <swing.sophie.8202@gmail.com>
2021-06-05 17:23:07 -0700

    commit tree 6644cdbeed09a0fd54a7303e8f8543150d671236

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This is really cool!"
+  );
+  
